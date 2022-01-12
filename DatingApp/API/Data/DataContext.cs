fe585ea99943351e113fa1db7ee5f6b51560b8f2
DekelA <dekel16791@gmail.com>
2022-01-12 19:26:12 +0200

    Update1.0
    Adding API

---
+++ DatingApp/API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        #region Constructors
+
+        public DataContext(DbContextOptions options): base(options)
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public DbSet<AppUser> Users { get; set; }
+
+        #endregion
+    }
+}
