68a025d73a3b92d60562731e328c819cd06fddf4
DekelA <dekel16791@gmail.com>
2022-01-15 01:47:34 +0200

    Update
    Adding angular routing

--- backend/DatingApp/API/Controllers/UsersController.cs
+++ backend/DatingApp/API/Controllers/UsersController.cs
@@ -3,9 +3,7 @@ using API.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Controllers
